43689ab4297171e5533ccd7d4662fe6d88b63b32
Evgeniy Fayzrakhmanov <Jonnynsk@mail.ru>
2021-11-23 02:25:40 +0700

    feat: start project

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const App = () => {
+	return <h1>Rick and Morty</h1>
+}
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    modules: [path.join(__dirname, 'src'), 'node_modules'],
+    alias: {
+      react: path.join(__dirname, 'node_modules', 'react'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
